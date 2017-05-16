60ad6f7ac39bf01e6d5c53e2b055d040b6eb5ee6
fejuchi <fejuchi@hotmail.com>
2017-05-15 22:57:11 -0600

    actualizacion

---
+++ registrar_cliente.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+session_start();
+if($_SESSION['ok']=="ok")
+{
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Registrar Cliente</title>
+<link rel="stylesheet" type="text/css" href="css/view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
+</head>
+
+<body id="main_body" >
+	<img id="top" src="css/top.png" alt="">
+	<div id="form_container">
+	
+		<h1><a>Registrar Cliente</a></h1>
+		<form id="form_1075005" class="appnitro"  method="post" action="registrar_cliente2.php">
+			<div class="form_description">
+			<h2>Registrar Cliente</h2>
+			<p></p>
+			</div>
+
+	<ul>
+		<li id="li_1" >
+		<label class="description" for="element_1">NIT </label>
+		<div>
+			<input id="nit_cliente" name="nit_cliente" class="element text medium" type="text" maxlength="255" value="" required /> 
+		</div> 
+		</li>
+		<li id="li_2" >
+		<label class="description" for="element_2">Nombre </label>
+		<span>
+			<input id="nombre" name= "nombre" class="element text" maxlength="255" size="20" value="" required />
+			<label>Nombres</label>
+		</span>
+		<span>
+			<input id="apellido" name= "apellido" class="element text" maxlength="255" size="20" value="" required />
+			<label>Apellidos</label>
+		</span> 
+		</li>		
+		<li id="li_3" >
+		<label class="description" for="element_3">Direccion </label>
+		<div>
+			<input id="direccion" name="direccion" class="element text medium" type="text" maxlength="255" value="" required /> 
+		</div> 
+		</li>
+		<li id="li_4" >
+		<label class="description" for="element_4">Telefono </label>
+		<div>
+			<input id="telefono" name="telefono" class="element text medium" type="text" maxlength="255" value="" required /> 
+		</div> 
+		</li>		
+		<li id="li_5" >
+		<label class="description" for="element_5">Email </label>
+		<div>
+			<input id="email" name="email" class="element text medium" type="text" maxlength="255" value="" required /> 
+		</div> 
+		</li>	
+
+		<select name="" id="">
+			<?php
+			include("conexion.php");	
+			$con=conectarse();
+			$sql="SELECT codigo_mun, nombre FROM municipio";
+			$stmt=$con->prepare($sql);
+			$result=$stmt->execute();
+			$rows=$stmt->fetchAll(\PDO::FETCH_OBJ);
+			foreach ($rows as $row) {
+			?>
+			<option value="<?php print($row->nombre); ?>"><?php print($row->nombre); ?></option>
+				<?php 
+				}
+			?>
+		</select>
+
+		<li class="buttons">
+		<input type="hidden" name="form_id" value="1075005" />
+		<input id="saveForm" class="button_text" type="submit" name="submit" value="Registrar" />
+		</li>
+	</ul>
+		</form>	
+		<div id="footer">
+		</div>
+	</div>
+	<img id="bottom" src="css/bottom.png" alt="">
+	</body>
+
+<?php
+}
+else
+{
+	header("location: index.php");
+}
+?>
+</html>
---
+++ registrar_cliente2.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head> 
+<title> REGISTRAR USUARIOS </title>
+<html lang="es">
+<meta charset="UTF-8">
+</head>
+<body bgcolor="#FF8000">
+<style>
+h1  {color:#0174DF}
+</style>
+
+<?php
+if(isset($_POST['submit']))
+{
+include("conexion.php");
+$con=conectarse();
+
+$nit_cliente=$_POST['nit_cliente'];
+$nombre=$_POST['nombre'];
+$apellido=$_POST['apellido'];
+$direccion=$_POST['direccion'];
+$telefono=$_POST['telefono'];
+$email=$_POST['email'];
+$codigo_mun=$_POST['cogigo_mun'];
+
+$result=$con->query("INSERT INTO cliente (nit_cliente, nombre, apellido, direccion,  telefono, email, codigo_mun) 
+	VALUES ('$nit_cliente', '$nombre', '$apellido', '$direccion', '$telefono, '$email', '$codigo_mun')");
+
+if($result>=1)
+{
+	echo "<center><h1> DATOS ALMACENADOS CON EXITOS</h1></center>";
+}
+else
+{
+	echo "<center><h1>SE HA PRODUCIDO UN ERROR</h1></center>";
+}
+}
+?>
+<meta http-equiv='refresh' content='1; url=registrar_cliente.php' />
+</body>
+</html>
